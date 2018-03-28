10dcf646a06f0df03699df46d3df26ad17f7fa61
at-vietnguyen <viet.nguyen@asiantech.vn>
2018-03-28 17:12:56 +0700

    abc

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
-set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
